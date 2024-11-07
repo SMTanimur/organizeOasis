d179384c07e27eceb090b44abb2a6a57d0d50bc3
SM Tanimur Rahman <smtanimurrahman404@gmail.com>
2024-11-07 17:12:49 +0600

    feat: :hammer: enhance CORS configuration and improve code formatting in SocketGateway

--- src/modules/gateway/socket.gateway.ts
+++ src/modules/gateway/socket.gateway.ts
@@ -14,17 +14,17 @@ import { ChatService } from '../chats/chat.service';
 import { STATUS, User } from '../users/schema/user.schema';
 import { ChatEvent } from '../chats/chat.enum';
 
-
 @WebSocketGateway({
-	cors: {
-		origin: [
-			'http://localhost:3000',
-      'https://project-management-five-delta.vercel.app/'
-		],
-		credentials: true,
-	},
-	// namespace: 'chat',
-	// transports: ['websocket'],
+  cors: {
+    origin: [
+      'http://localhost:3000',
+      'https://project-management-five-delta.vercel.app',
+    ],
+    methods: ['GET', 'POST'],
+    allowedHeaders: ['Authorization', 'Content-Type'],
+    credentials: true,
+  },
+ 
 })
 @UseGuards(WsAuthGuard)
 export class SocketGateway implements OnGatewayConnection, OnGatewayDisconnect {
@@ -39,14 +39,11 @@ export class SocketGateway implements OnGatewayConnection, OnGatewayDisconnect {
   }
 
   async handleConnection(client: Socket) {
-   
-    
     // Check if user data is available
-    const userId = client.handshake.query.userId
- 
-   
+    const userId = client.handshake.query.userId;
+
     const organizationId = client.handshake.query.organizationId as string;
-  
+
     if (!organizationId) {
       client.disconnect(true);
       return;
@@ -54,27 +51,33 @@ export class SocketGateway implements OnGatewayConnection, OnGatewayDisconnect {
 
     console.log('userId:', userId);
 
-  
     // Join the user's room
     client.join(`user_${userId}`);
-  
+
     try {
-      const chats = await this.chatsService.getUserChats(userId as string, organizationId, { page: 1, limit: 10 });
-       // Join each chat room
-    chats.data.forEach((chat) => {
-      client.join(`chat_${chat._id}`);
-    });
+      const chats = await this.chatsService.getUserChats(
+        userId as string,
+        organizationId,
+        { page: 1, limit: 10 },
+      );
+      // Join each chat room
+      chats.data.forEach((chat) => {
+        client.join(`chat_${chat._id}`);
+      });
       console.log('Chats fetched in socket:', chats);
     } catch (error) {
       console.error('Error fetching chats in socket:', error);
     }
-   
-  
+
     // Update user status to online
-    await this.chatsService.handleUpdateUserStatus(userId as string, STATUS.ONLINE, new Date());
-  
+    await this.chatsService.handleUpdateUserStatus(
+      userId as string,
+      STATUS.ONLINE,
+      new Date(),
+    );
+
     // Emit the user's online status to all clients
-   this.server.emit('userStatusChanged', {
+    this.server.emit('userStatusChanged', {
       userId: userId,
       status: STATUS.ONLINE,
     });
@@ -82,7 +85,7 @@ export class SocketGateway implements OnGatewayConnection, OnGatewayDisconnect {
 
   async handleDisconnect(client: Socket) {
     // Check if user data is available
-    const userId = client.handshake.query.userId
+    const userId = client.handshake.query.userId;
 
     // Update user status to offline
     await this.chatsService.handleUpdateUserStatus(
@@ -103,8 +106,8 @@ export class SocketGateway implements OnGatewayConnection, OnGatewayDisconnect {
     @ConnectedSocket() client: Socket,
     @MessageBody() chatId: string,
   ) {
-    const userId = client.handshake.query.userId
-   
+    const userId = client.handshake.query.userId;
+
     await this.chatsService.validateChatMember(chatId, userId as string);
     client.join(`chat_${chatId}`);
     return { event: ChatEvent.JOIN, chatId };
@@ -124,7 +127,7 @@ export class SocketGateway implements OnGatewayConnection, OnGatewayDisconnect {
     @ConnectedSocket() client: Socket,
     @MessageBody() data: { chatId: string; isTyping: boolean },
   ) {
-    const userId = client.handshake.query.userId
+    const userId = client.handshake.query.userId;
     client.broadcast.to(`chat_${data.chatId}`).emit(ChatEvent.TYPING, {
       chatId: data.chatId,
       userId: userId,
@@ -137,7 +140,7 @@ export class SocketGateway implements OnGatewayConnection, OnGatewayDisconnect {
     @ConnectedSocket() client: Socket,
     @MessageBody() data: { chatId: string; messageIds: string[] },
   ) {
-    const userId = client.handshake.query.userId
+    const userId = client.handshake.query.userId;
     await this.chatsService.markMessagesAsRead(
       data.chatId,
       data.messageIds,
